6762ff848f4d548b1aaab54d7c106c233a52eefc
ojiepermana <me@ojiepermana.com>
2021-01-06 18:59:04 +0700

    setup firebase hosting config key

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'template';
+export class AppComponent  implements OnInit {
+  data = {
+    name: 'Michael Jordan',
+    bio: 'Former baseball player',
+    image: 'avatar.png'
+  };
+
+  constructor(private title: Title, private meta: Meta) {}
+
+  ngOnInit() {
+    this.title.setTitle(this.data.name);
+    this.meta.addTags([
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'og:url', content: '/about' },
+      { name: 'og:title', content: this.data.name },
+      { name: 'og:description', content: this.data.bio },
+      { name: 'og:image', content: this.data.image }
+    ]);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from '@angular/fire';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { LayoutModule } from 'src/layout/layout.module';
+
+const config = {
+  apiKey: "AIzaSyDVACPtgpbRKhc9OhXBr69ntqaJ6oH34Ys",
+  authDomain: "acpt-frend.firebaseapp.com",
+  projectId: "acpt-frend",
+  storageBucket: "acpt-frend.appspot.com",
+  messagingSenderId: "659831005205",
+  appId: "1:659831005205:web:a605df5e5f68af966a6e1c"
+};
 
 @NgModule({
   declarations: [
@@ -12,6 +23,7 @@ import { LayoutModule } from 'src/layout/layout.module';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AngularFireModule.initializeApp(config),
     AppRoutingModule,
     FontAwesomeModule,
     LayoutModule
